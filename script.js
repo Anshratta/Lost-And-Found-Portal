e23d8e3b0caed7fe19c18ef5967c664ba16350f2
Anshratta <ansh.r15057@gmail.com>
2026-06-21 19:13:40 +0530

    Added search functionality

--- script.js
+++ script.js
@@ -59,3 +59,95 @@ localStorage.setItem(
     form.reset();
 
 });
+const foundForm =
+document.getElementById("foundForm");
+
+const foundContainer =
+document.getElementById("foundItemsContainer");
+
+const foundCount =
+document.getElementById("foundCount");
+
+let totalFound =
+Number(localStorage.getItem("totalFound")) || 0;
+
+foundCount.textContent = totalFound;
+
+foundContainer.innerHTML =
+localStorage.getItem("foundReports") || "";
+
+foundForm.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    const inputs =
+    foundForm.querySelectorAll("input");
+
+    const itemName = inputs[0].value;
+    const location = inputs[1].value;
+    const date = inputs[2].value;
+    const contact = inputs[3].value;
+
+    const description =
+    foundForm.querySelector("textarea").value;
+
+    const card =
+    document.createElement("div");
+
+    card.classList.add("item-card");
+
+    card.innerHTML = `
+        <h3>${itemName}</h3>
+        <p><b>Location:</b> ${location}</p>
+        <p><b>Date:</b> ${date}</p>
+        <p><b>Contact:</b> ${contact}</p>
+        <p>${description}</p>
+    `;
+
+    foundContainer.appendChild(card);
+
+    totalFound++;
+
+    foundCount.textContent =
+    totalFound;
+
+    localStorage.setItem(
+        "totalFound",
+        totalFound
+    );
+
+    localStorage.setItem(
+        "foundReports",
+        foundContainer.innerHTML
+    );
+
+    foundForm.reset();
+
+});
+const searchInput =
+document.getElementById("searchInput");
+
+searchInput.addEventListener("keyup", function(){
+
+    const value =
+    searchInput.value.toLowerCase();
+
+    const cards =
+    document.querySelectorAll(".item-card");
+
+    cards.forEach(card => {
+
+        if(
+            card.textContent
+            .toLowerCase()
+            .includes(value)
+        ){
+            card.style.display = "block";
+        }
+        else{
+            card.style.display = "none";
+        }
+
+    });
+
+});
